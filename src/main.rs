1d26f8be621a1a68ee3997dcbc9842c701fa4786
Ryan <rruckley@gmail.com>
2023-09-02 19:01:13 +1000

    detect some edges

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@
 use log::info;
 use anyhow::Result;
 use opencv::{
+    self as cv,
     prelude::*,
-    videoio,
-    highgui
+    highgui,
+    imgproc,
 };
 
 fn main() -> Result<()> {
@@ -18,15 +19,33 @@ fn main() -> Result<()> {
 
     highgui::named_window(pkg, highgui::WINDOW_FULLSCREEN)?;
 
-    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    //let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
 
     let mut frame = Mat::default();
 
+    // Read the image
+    let img = opencv::imgcodecs::imread("boulder1.jpg", cv::imgcodecs::IMREAD_GRAYSCALE)?;
+
+    // Seem pretty close
+    let mut thresh1= 51.0;
+    let mut thresh2 = 75.0;
+
     loop {
-        cam.read(&mut frame)?;
+        info!("Now using thresholds {} , {}",thresh1, thresh2);
+        imgproc::canny(
+            &img,
+            &mut frame,
+            thresh1,
+            thresh2,
+            3,
+            true
+        )?;
         highgui::imshow(pkg, &frame)?;
-        let key = highgui::wait_key(1)?;
-        if key == 113 {
+        thresh1 += 5.0;
+        thresh2 += 6.0;
+        
+        let key = highgui::wait_key(5000)?;
+        if key == 113 { // quit with q
             break;
         }
     }
